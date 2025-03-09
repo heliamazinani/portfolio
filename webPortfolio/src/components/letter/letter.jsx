4354441edbf036139a78ce2827ee2a69c26ffce6
HeliaMazinani <13helia1383@gmail.com>
2025-03-09 19:13:17 +0330

    commision fron completed

--- webPortfolio/src/components/letter/letter.jsx
+++ webPortfolio/src/components/letter/letter.jsx
@@ -1,17 +1,50 @@
-import React from "react";
-import "./letter.css"
+import React, { useEffect, useState } from "react";
+import "./letter.css";
+import letterback from "../../assets/letter-back.svg";
+import letterfront from "../../assets/letter-front.svg";
+
 const Letter = () => {
+  const [animate, setAnimate] = useState(false);
 
-    return(<div className="letterkh">
-        <div className="innerletter">
-        write Your idea here
-        <textarea name="message" rows="8" placeholder="Your idea"></textarea>
-        <button>
-            upload refrences
-        </button>
-        <button>submit</button>
+  useEffect(() => {
+    if(animate){
+  
+      const element = document.getElementById("ss");
+      if (element) {
+        console.log('Element found:', element); 
+
+        element.style.transition = 'transform 1s ease';
+        element.style.transform = 'translateY(350px)';
+
+
+      }
+    }
+  }, [animate]);
+
+  return (
+    <div className="letterkh">
+      <div id="let-container">
+        <img className="let1" src={letterback} alt="Letter Back"></img>
+        <div id="ss" className="innerletter">
+
+          <div className="help">
+            <textarea className="message" rows="8" placeholder="Write Your idea"></textarea>
+            <button className="upload">upload references</button>
+            <button 
+              className="go" 
+              onClick={() => {
+                console.log('Submit button clicked'); 
+                setAnimate(true);
+              }}
+            >
+              send
+            </button>
+          </div>
+        </div>
+        <img className="let" src={letterfront} alt="Letter Front"></img>
+      </div>
     </div>
-    </div>);
+  );
+};
 
-}
-export default Letter
+export default Letter;
